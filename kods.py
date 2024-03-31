46a9e4f13cb6db2ee6b8897eee2ab936ecb07d4b
alinanc076 <163727521+alinanc076@users.noreply.github.com>
2024-03-31 13:41:48 +0300

    Create kods.py

---
+++ kods.py
@@ -0,0 +1,28 @@
+import discord
+import kod2
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$merhaba'):
+        await message.channel.send("Selam!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('Parola önerebilir misin '):
+        await message.channel.send(kod2.x(10))
+
+    else:
+        await message.channel.send(message.content)
+
+client.run("Token")
